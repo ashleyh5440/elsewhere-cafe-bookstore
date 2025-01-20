35341b7bc6f62137fe2bdc0f25e8fb7591bc9bbd
Ashley <ashleyh5440@gmail.com>
2025-01-19 19:59:56 -0500

    changed colors around

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,5 +1,6 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import { NavLink } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu'
 
@@ -7,40 +8,46 @@ import './style.css';
 
 function Navigation({ routes }) {
   return (
-    <Navbar expand="lg" style={{ backgroundColor: "rgb(7, 64, 7)" }}>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mx-auto nav-center">
-      {routes.map((route) => (
-        <Nav.Link
-          key={route.path}
-          as={NavLink}
-          to={route.path}
-          className={({ isActive }) => (isActive ? 'active nav-link-spacing' : 'nav-link-spacing')}
-          end
-        >
-          {route.name}
-        </Nav.Link>
-      ))}
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-
-    // <Navbar expand="lg" style={{ backgroundColor: "rgb(7, 64, 7)" }}>
-    //   <Nav className="mx-auto nav-center">
-    //     {routes.map((route) => (
-    //       <Nav.Link
-    //         key={route.path}
-    //         as={NavLink}
-    //         to={route.path}
-    //         className={({ isActive }) => (isActive ? 'active nav-link-spacing' : 'nav-link-spacing')}
-    //         end
-    //       >
-    //         {route.name}
-    //       </Nav.Link>
-    //     ))}
-    //   </Nav>
-    // </Navbar>
+    <Navbar expand="lg" style={{ backgroundColor: "#301933" }}>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mx-auto nav-center">
+          {routes.map((route) => {
+            if (route.mae === 'Shopping') {
+              return (
+                <NavDropdown
+                  title={route.name}
+                  id="shopping-dropdown"
+                  key={route.path}
+                  className="nav-link-spacing"
+                >
+                  <NavDropdown.Item as={NavLink} to="/shopping/books">
+                    Books
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={NavLink} to="/shopping/ingredients">
+                    Ingredients
+                  </NavDropdown.Item>
+                </NavDropdown>
+              );
+            } else {
+              return (
+                <Nav.Link 
+                  key={route.path}
+                  as={NavLink}
+                  to={route.path}
+                  className={({ isActive }) =>
+                  isActive ? 'active nav-link-spacing' : 'nav-link-spacing'
+                }
+                end 
+                >
+                  {route.name}
+                </Nav.Link>
+              );
+            }
+          })}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
